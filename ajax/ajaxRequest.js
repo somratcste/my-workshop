e10407afb014858482e40d9a05eb15451653ec6f
G. M. Nazmul Hossain <somratcste@gmail.com>
2017-07-03 10:58:43 +0000

    for phalcon request

--- ajax/ajaxRequest.js
+++ ajax/ajaxRequest.js
@@ -6,3 +6,24 @@ $.ajax({
 		$('#showlist').html(data);
 	}
 });
+
+
+//phalcon 
+var url = '/npfadmin/contenttype/croppie';
+$.ajax({
+	url: url,
+	type: 'POST',
+	dataType : 'json',
+	data: {imagebase64 : resp, contenttype : '<?php echo $contenttype; ?>', uploadPath : $('#uploadpath').val(), previousImage : $('#imageID').attr('src')},
+	success: function(response){
+		$('#myModal').modal('toggle');
+		$('.upload_image').show();
+		$('#imageID').attr('src',response.result);
+		$('#_0_name').val(response.filename);
+		alert("Image Upload Successfully");
+	},
+	error: function(response){
+		$('#myModal').modal('toggle');
+		alert(response);
+	}
+});
