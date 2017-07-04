bca6c72bb671d6a86e0d92cd681944787c77727e
G. M. Nazmul Hossain <somratcste@gmail.com>
2017-07-04 08:34:01 +0000

    update modal view

--- croppie/image_croppie.php
+++ croppie/image_croppie.php
@@ -1,33 +1,51 @@
 {{ stylesheet_link('css/croppie.css') }}
 {{ javascript_include('js/croppie.js') }}
+<style type="text/css">
+	.modal {
+		background-clip: padding-box;
+		background-color: #fff;
+		border: 1px solid rgba(0, 0, 0, 0.3);
+		border-radius: 6px;
+		box-shadow: 0 3px 7px rgba(0, 0, 0, 0.3);
+		left: 35%;
+		margin: -250px 0 0 -280px;
+		outline: 0 none;
+		position: fixed;
+		top: 50%;
+		width: 900px;
+		z-index: 1050;
+	}
+	.modal.fade.in {
+	  top: 40%;
+	}
+</style>
 
-<div class="twelve column">
-	<!-- <button type="button" class="btn btn-info btn-lg" data-toggle="modal" data-target="#myModal">Upload Image</button> -->
-	<!-- Modal -->
-	<div id="myModal" class="modal fade" role="dialog">
-	  <div class="modal-dialog">
-	    <div class="modal-content">
-	      <div class="modal-header btn-info">
-	        <button type="button" class="close" data-dismiss="modal">&times;</button>
-	        <h4 class="modal-title">Upload Your Image</h4>
-	      </div>
-	      <div class="modal-body">
-	        <form>
-		        <input type="file" id="upload" value="Choose a file">
-		        <div id="upload-demo"></div>
-		        <input type="hidden" id="imagebase64" name="imagebase64">
-	      </div>
-	      <div class="modal-footer btn-info">
-	      	<button type="button" class="upload-result btn btn-success">Upload</button>
-	        <button type="button" class="btn btn-danger" data-dismiss="modal">Close</button>
-	      </div>
-	        </form>
-	    </div>
+<!-- <button type="button" class="btn btn-info btn-lg" data-toggle="modal" data-target="#myModal">Upload Image</button> -->
+<!-- Modal -->
+<div id="myModal" class="modal fade modal-lg" role="dialog">
+  <div class="modal-dialog modal-xl">
+    <div class="modal-content">
+      <div class="modal-header btn-info">
+        <button type="button" class="close" data-dismiss="modal">&times;</button>
+        <h4 class="modal-title">Upload Your Image </h4>
+      </div>
+      <div class="modal-body">
+        <form>
+	        <input type="file" id="upload" value="Choose a file">
+	        <div id="upload-demo"></div>
+	        <input type="hidden" id="imagebase64" name="imagebase64">
+      </div>
+      <div class="modal-footer btn-info">
+      	<button type="button" class="upload-result btn btn-success">Upload</button>
+        <button type="button" class="btn btn-danger" data-dismiss="modal">Close</button>
+      </div>
+        </form>
+    </div>
 
-	  </div>
-	</div>
-	<!--End Modal -->
+  </div>
 </div>
+<!--End Modal -->
+
 
 <table id="tbl-{{ fldname }}">
     <thead>
@@ -46,8 +64,8 @@
                 <div class="input-append">
                     {{ text_field(fldname~"[0][name]","id":fldname~"_0_name","class":"input-medium","readonly":"readonly","value":fldval['name']) }}
                     {{ hidden_field(fldname~"[0][path]","value":fldval['path']) }}
-                    <a class="btn" data-toggle="modal" data-target="#myModal"><i class="icon-folder-open"></i></a>
-                    <a id="btn-contentref" href="javascript:;" role="button" class="btn" onclick="clearFldVal('#{{ fldname }}_0_name'); return false;"><i class="icon-remove"></i></a>
+                    <a class="btn" id="showModal" data-toggle="modal" data-target="#myModal"><i class="icon-folder-open"></i></a>
+                    <a id="imageRemove" role="button" class="btn"><i class="icon-remove"></i></a>
                 </div>
             </td>
             <td>{{ text_field(fldname~"[0][caption_bn]","value":fldval['caption_bn']) }}</td>
@@ -67,8 +85,8 @@
                 <div class="input-append">
                     {{ text_field(fldname~"[0][name]","id":fldname~"_0_name", "class":"input-medium","readonly":"readonly") }}
                     {{ hidden_field(fldname~"[0][path]","value":"") }}
-                    <a class="btn" data-toggle="modal" data-target="#myModal"><i class="icon-folder-open"></i></a>
-                    <a id="btn-contentref" href="javascript:;" role="button" class="btn" onclick="clearFldVal('#{{ fldname }}_0_name'); return false;"><i class="icon-remove"></i></a>
+                    <a class="btn" id="showModal" data-toggle="modal" data-target="#myModal"><i class="icon-folder-open"></i></a>
+                    <a id="imageRemove" role="button" class="btn"><i class="icon-remove"></i></a>
                 </div>
             </td>
             <td>{{ text_field(fldname~"[0][caption_bn]") }}</td>
@@ -107,12 +125,12 @@
 
         $uploadCrop = $('#upload-demo').croppie({
             viewport: {
-                width: 200,
-                height: 200
+                width: {{ width }},
+                height: {{ height }}
             },
             boundary: {
-                width: 250,
-                height: 250
+                width: 800,
+                height: 300
             }
         });
 
@@ -122,8 +140,6 @@
                 type: 'canvas',
                 size: 'original'
             }).then(function (resp) {
-            	$('#upload-demo').hide();
-            	$("#upload").val('');
                 $('#imagebase64').val(resp);
                 var url = '/npfadmin/contenttype/croppie';
 		        $.ajax({
@@ -145,5 +161,33 @@
 				});
             });
         })
+
+        $('#imageRemove').on('click', function(){
+        	if($('#imageID').attr('src')){
+	        	var url = '/npfadmin/contenttype/croppieCancel';
+	        	$.ajax({
+			        	url: url,
+						type: 'POST',
+						dataType : 'json',
+						data: {previousImage : $('#imageID').attr('src')},
+						success: function(response){
+							$('#_0_name').val('');
+	        				$('.upload_image').hide();
+							alert(response.result);
+						},
+						error: function(response){
+							console.log(response);
+							alert("Image can not deleted.");
+						}
+					});
+        	} else {
+        		return false;
+        	}
+        });
+
+        $('#showModal').on('click', function(){
+        	$('#upload-demo').hide();
+            $("#upload").val('');
+        });
     });
 </script>
