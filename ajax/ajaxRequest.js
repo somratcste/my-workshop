c42de91600e9fc0919049f037116b6152ade9747
DESKTOP-IF64SKA\somrat <somratcste@gmail.com>
2017-09-13 12:07:22 +0600

    ajax with cache

--- ajax/ajaxRequest.js
+++ ajax/ajaxRequest.js
@@ -35,3 +35,36 @@ $.ajax({
 		alert(response);
 	}
 });
+
+
+// cakephp 3 ajax with cache 
+
+$(function () {
+        $('[name=part_number]').on("change", function () {
+            var val = $(this).val();
+            if (val.length > 0) {
+                $.ajax({
+                    cache: true,
+                    url: '<?= $this->Url->build(["_name" => 'partDetail']) ?>',
+                    method: 'post',
+                    dataType: 'json',
+                    data: {'part_number': val},
+                    success: function (response) {
+                        if (typeof response.part != 'undefined' && Object.keys(response.part).length != 0) {
+                            $.each(response.part, function (i, v) {
+                                $('[name=' + i + ']').val(v)
+                            });
+                            $('.alert-info').show();
+                            $('.show-alert').append("The Part Already Exist . <a href='/mpms_new/part_infos/view/"+response.part.id+"' class='icon-eye'> | </a><a href='/mpms_new/part_infos/edit/"+response.part.id+"' class='icon-pencil3'>");
+                        } else {
+                            $('[name=id]').val(0);
+                            $('.form').find('input[type=text]').not('[name=part_number]').val('');
+                            $('.form').find('select').val('');
+                            $('.form').find('textarea').val('');
+                        }
+                    }
+                });
+            }
+
+        })
+    });
